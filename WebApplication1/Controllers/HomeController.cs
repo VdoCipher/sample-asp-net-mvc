b1f5fa5e90d325e996eafe12282154039fe3f8fe
Vibhav Sinha <v@vsinha.me>
2016-02-08 20:49:35 +0530

    upload action added

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -42,6 +42,31 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public ActionResult Upload()
+        {
+            ViewBag.Message = "Upload new videos here";
+
+            string new_video_title = "VIDEO_TITLE";          // This should be obtained from DB
+            string api_secret = System.Web.Configuration.WebConfigurationManager.AppSettings["VdoCipher_API_Key"]; ;
+            string uri = "https://api.vdocipher.com/v2/uploadPolicy/";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            using (StreamWriter writer = new StreamWriter(request.GetRequestStream(), Encoding.ASCII))
+            {
+                writer.Write("clientSecretKey=" + api_secret + "&title=" + new_video_title);
+            }
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            dynamic otp_data;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string json_otp = reader.ReadToEnd();
+                otp_data = JObject.Parse(json_otp);
+            }
+            ViewBag.upload_data = otp_data;
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
